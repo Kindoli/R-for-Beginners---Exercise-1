348df2d254af7166330f5b61c1c455da3948bde1
Kindoli Edward <132260311+Kindoli@users.noreply.github.com>
2025-05-19 15:08:35 +0300

    Add files via upload
    Practical Exercises

---
+++ R for Beginners.R
@@ -0,0 +1,93 @@
+#################
+#KINDOLI EDWARD  #
+#R Tutorial      #
+#Data Conslutant #
+################
+
+## Exercise 1
+
+## In this exercise, you will practice doing exploratory data analysis and plotting in R.
+
+
+## -----------------------------------------------------------
+
+## 1. First, load the R package you will use in this exercise.
+library(readxl)
+library(tidyverse)
+library(readr)
+
+## Read in the Excel file called "Titanic dataset", and assign it to a dataset called "data.1". 
+## Hint: make sure you know what folder the file is saved in on your computer, and make sure you set your working directory to that folder.
+
+
+
+
+## 2. convert the variables into dates and factors. This will make downstream analyses easier:
+
+
+## 3. How many rows (observations) are in this dataset?
+
+
+
+
+## 4. How many columns (variables) are in this dataset?
+
+
+
+## 5. What are the names of the columns in this dataset?
+
+
+
+## 6. What is the value in Row 3, Column 4?
+
+
+
+
+## 7. What is the value in Row 4, Column 3?
+
+
+
+
+## 8. What are the oldest and youngest passengers included in this data set?
+## Hint: Get a summary of the "Age" variable.
+
+
+
+
+
+## 9. What is the mean age of participants included in this data set?
+
+
+
+## 10. Make a histogram of the ages of participants included in this data set.
+## Save this figure as a .png file called "hist_ageyrs".
+
+
+
+## 11. How many male and female are in the dataset ("sex")?
+
+
+
+
+
+## 12. How many Passengers were reported as survived? How many Passengers never survived?
+
+
+
+
+
+## 13. How many Passengers who survived were in first class ("Pclass")?
+
+
+
+
+
+## 14. Make a frequency (contigency) table of the Survived and Pclass variables. What do each of the three values in this table mean?
+
+
+
+
+
+## 15. Make a scatterplot of ageyrs against Fare, with the points as filled circles representing sex 
+# Save this scatterplot as a .pdf file called "scatter".
+
